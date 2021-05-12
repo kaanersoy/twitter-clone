0d0f5cd84eb386326d0fc368b1144287897501ec
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 00:38:29 +0300

    Create base icon for dynamic icon component

---
+++ src/components/BaseIcon/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<script>
+
+const icons = {
+  home: require('./icons/home').default,
+  bookmarks: require('./icons/bookmarks').default,
+  explore: require('./icons/explore').default,
+  lists: require('./icons/lists').default,
+  messages: require('./icons/messages').default,
+  more: require('./icons/more').default,
+  notifications: require('./icons/notifications').default,
+  profile: require('./icons/profile').default,
+}
+
+export default {
+  name: 'BaseIcon',
+  components: {
+    ...icons
+  },
+  props: {
+    icon: {
+      type: String,
+      default: 'home'
+    }
+  },
+  computed: {
+    iconComponent(){
+      return icons[this.icon]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
