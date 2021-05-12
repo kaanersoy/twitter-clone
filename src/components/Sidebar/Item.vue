acac69eaf7ac3c3566ee6ad8df34d5a1ffce8f1b
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 00:59:59 +0300

    Add sidebarItem component

---
+++ src/components/Sidebar/Item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="sidebar-item">
+    <div class="sidebar-item-logo">
+      <base-icon
+        :icon="icon"
+        :icon-color="iconColor"
+      />
+    </div>
+    <div class="sidebar-item-content" />
+  </div>
+</template>
+
+<script>
+import BaseIcon from '@/components/BaseIcon';
+
+export default {
+  name: 'SidebarItem',
+  components: {
+    BaseIcon
+  },
+  props: {
+    icon:{
+      type:String,
+      default: "more"
+    },
+    iconColor:{
+      type:String,
+      default: "#fff"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .sidebar-item{
+  display: flex;
+  &-logo{
+    width: 26px;
+    height: 26px;
+    svg{
+      fill: #fff;
+    }
+  }
+ }
+</style>
