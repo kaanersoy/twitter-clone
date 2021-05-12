887ba5af4430697a87fc514f500e8e23544e98ff
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 01:46:40 +0300

    Add constants to map all routes from one file

---
+++ src/components/Sidebar/constants.js
@@ -0,0 +1,26 @@
+export const ROUTES = [
+  {
+    name: 'Home',
+    path: '/home'
+  },
+  {
+    name: 'Explore',
+    path: '/explore'
+  },
+  {
+    name: 'Notifications',
+    path: '/notifications'
+  },
+  {
+    name: 'Messages',
+    path: '/messages'
+  },
+  {
+    name: 'Bookmarks',
+    path: '/bookmarks'
+  },
+  {
+    name: 'Lists',
+    path: '/lists'
+  }
+]
--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,30 +1,61 @@
 <template>
   <aside>
     <div class="sidebar-logo">
-      <base-icon icon="twitter" />
+      <router-link to="/home">
+        <base-icon icon="twitter" />
+      </router-link>
     </div>
-    <sidebar-item icon="home" />
+    <sidebar-item
+      v-for="(route, i) in ROUTES"
+      :key="i"
+      :icon="route.name.toLowerCase()"
+      :to="route.path"
+    >
+      {{ route.name }}
+    </sidebar-item>
   </aside>
 </template>
 
 <script>
 import SidebarItem from '@/components/Sidebar/Item'
+import { ROUTES } from '@/components/Sidebar/constants.js'
 import BaseIcon from '@/components/BaseIcon'
+
 
 export default {
   name: 'Sidebar',
   components:{
     SidebarItem,
     BaseIcon
-  }
+  },
+  data: function(){
+    return {
+      ROUTES
+    }
+  },
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/theme/colors.scss';
+
 aside{
   .sidebar{
     &-logo{
-
+      width: 2rem;
+      height: 2rem;
+      padding: 12px;
+      border-radius: 999px;
+      transition: 200ms ease-in-out;
+      svg{
+        fill: #fff;
+      }
+      &:hover{
+        background-color: rgba($color: $color-blue, $alpha: 0.2);
+        svg{
+          fill: #fff;
+        }
+      }
     }
   }
 }
