237998d28e15891c4c281e5e814e09a7e0473e71
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-12 23:26:36 +0300

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+      browser: true,
+      node: true,
+      es6: true
+      // 'jest/globals': true
+    },
+    extends: ['plugin:vue/recommended', 'eslint:recommended'],
+    // plugins: ['jest'],
+}
