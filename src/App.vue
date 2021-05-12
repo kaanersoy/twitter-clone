a5babbdf3bb1f7425b265159bf6eb6941053fc1d
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-12 22:38:14 +0300

    Add router for basic

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
-  <div>Hello</div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-}
-</script>
-
 <style lang="scss">
-@import './assets/theme/colors.scss';
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: $color-blue;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
