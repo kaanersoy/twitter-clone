a42527dd613f2ab8b850987952cda4ec81885dc5
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 00:59:45 +0300

    Update sidebar component

---
+++ src/components/Sidebar/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside>
+    <div class="sidebar-logo">
+      <base-icon icon="twitter" />
+    </div>
+    <sidebar-item icon="home" />
+  </aside>
+</template>
+
+<script>
+import SidebarItem from '@/components/Sidebar/Item'
+import BaseIcon from '@/components/BaseIcon'
+
+export default {
+  name: 'Sidebar',
+  components:{
+    SidebarItem,
+    BaseIcon
+  }
+}
+</script>
+
+<style lang="scss">
+aside{
+  .sidebar{
+    &-logo{
+
+    }
+  }
+}
+</style>
