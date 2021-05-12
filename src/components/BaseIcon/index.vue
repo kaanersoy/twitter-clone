9ff6f37b1d97dfa16e9afc67e5464fe3601f06c7
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 00:59:32 +0300

    Update baseIcon component for more generic version

--- src/components/BaseIcon/index.vue
+++ src/components/BaseIcon/index.vue
@@ -4,31 +4,17 @@
 
 <script>
 
-const icons = {
-  home: require('./icons/home').default,
-  bookmarks: require('./icons/bookmarks').default,
-  explore: require('./icons/explore').default,
-  lists: require('./icons/lists').default,
-  messages: require('./icons/messages').default,
-  more: require('./icons/more').default,
-  notifications: require('./icons/notifications').default,
-  profile: require('./icons/profile').default,
-}
-
 export default {
   name: 'BaseIcon',
-  components: {
-    ...icons
-  },
   props: {
     icon: {
       type: String,
       default: 'home'
-    }
+    },
   },
   computed: {
     iconComponent(){
-      return icons[this.icon]
+      return require(`./icons/${this.icon}`).default
     }
   }
 }
