281388e4417d2014091c1338d46ae55704915c50
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-12 23:25:13 +0300

    Initial commit for layout

---
+++ src/views/Layout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="layout">
+    <div class="layout-sidebar">
+      <sidebar />  
+    </div>
+    <div class="layout-flow">
+      <slot />
+    </div>
+    <div class="layout-for-you">
+      some-for-you-bar-goes-there
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+
+export default {
+  name: 'Layout',
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style lang="scss">
+  .layout{
+    max-width: 1320px;
+    display: flex;
+    &-sidebar{
+      width: 275px;
+    }
+    &-flow{
+      width: 650px;
+    }
+    &-for-you{
+      width: 300px;
+    }
+  }
+
+</style>
