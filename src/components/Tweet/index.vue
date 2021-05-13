19c4695654f25d686433f3c43acedd6122ea30b2
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 14:47:50 +0300

    Initial commit for tweet

---
+++ src/components/Tweet/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tweet">
+    <div class="tweet-owner">
+      <img src="https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_normal.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tweet',
+}
+</script>
+
+<style>
+
+</style>
