370b11e78662295c74b6729bddf84c35cae33da2
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 12:28:58 +0300

    Add jsconfig file

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
   },
