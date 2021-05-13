89e4ed245941c4853d1889c573ae541c3fbbccd6
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 20:32:40 +0300

    Add wrapper for navigation menu

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,31 +1,51 @@
 <template>
   <aside>
-    <div class="sidebar-logo">
-      <router-link to="/">
-        <base-icon icon="twitter" />
-      </router-link>
+    <div class="sidebar-nav">
+      <div class="sidebar-logo">
+        <router-link to="/">
+          <base-icon icon="twitter" />
+        </router-link>
+      </div>
+      <sidebar-item
+        v-for="(route, i) in ROUTES"
+        :key="i"
+        :icon="route.name.toLowerCase()"
+        :to="route.path"
+      >
+        {{ route.name }}
+      </sidebar-item>
+      <sidebar-item
+        icon="profile"
+        to="/profile"
+      >
+        Profile
+      </sidebar-item>
+      <sidebar-item
+        icon="more"
+        @click="toggleMenu"
+      >
+        More
+      </sidebar-item>
+      <more-menu v-if="isMenuOpened" />
     </div>
-    <sidebar-item
-      v-for="(route, i) in ROUTES"
-      :key="i"
-      :icon="route.name.toLowerCase()"
-      :to="route.path"
-    >
-      {{ route.name }}
-    </sidebar-item>
-    <sidebar-item
-      icon="profile"
-      to="/profile"
-    >
-      Profile
-    </sidebar-item>
-    <sidebar-item
-      icon="more"
-      @click="toggleMenu"
-    >
-      More
-    </sidebar-item>
-    <more-menu v-if="isMenuOpened" />
+    <div class="sidebar-profile">
+      <div class="sidebar-profile-pic">
+        <img
+          :src="me.profile.pic"
+        >
+      </div>
+      <div class="sidebar-profile-items">
+        <div class="profile-info">
+          <p>Kaan Ersoy</p>
+          <p class="nickname">
+            @kaanersoy
+          </p>
+        </div>
+        <div class="more">
+          <base-icon icon="more" />
+        </div>
+      </div>
+    </div>
   </aside>
 </template>
 
@@ -34,6 +54,7 @@ import SidebarItem from '@/components/Sidebar/Item'
 import { ROUTES } from '@/components/Sidebar/constants.js'
 import BaseIcon from '@/components/BaseIcon'
 import MoreMenu from '@/components/Sidebar/MoreMenu'
+import { mapGetters } from 'vuex'
 
 
 export default {
@@ -49,6 +70,11 @@ export default {
       isMenuOpened: false
     }
   },
+  computed: {
+    ...mapGetters({
+      me: 'getMe'
+    })
+  },
   methods: {
     toggleMenu: function(){
       this.isMenuOpened = !this.isMenuOpened;
@@ -63,7 +89,15 @@ export default {
 
 aside{
   position: relative;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   .sidebar{
+    &-nav{
+      position: relative;
+    }
+    height: 100%;
     &-logo{
       width: 50px;
       height: 50px;
@@ -81,6 +115,47 @@ aside{
           fill: #fff;
         }
       }
+    }
+    &-profile{
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding: 0 1rem;
+      border-radius: 999px;
+      &:hover{
+        background-color: rgba($color: $color-blue, $alpha: 0.3);
+      }
+      &-pic{
+        width: 3rem;
+        img{
+          border-radius: 999px;
+          width: 100%;
+        }
+      }
+      &-items{
+        margin-left: 10px;
+        display: flex;
+        flex-grow: 1;
+        align-items: center;
+        justify-content: space-between;
+        .profile-info{
+          p{
+            color: #fff;
+            &.nickname{
+              color: $color-dark-gray;
+            }
+          }
+        }
+        .more{
+          width: 2rem;
+          height: 2rem;
+          svg{
+            width: 100%;
+            fill: #fff;
+            display: block;
+          }
+        }
+      } 
     }
   }
 }
