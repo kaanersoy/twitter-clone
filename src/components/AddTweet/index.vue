8b0e0f1f733e16fc126ed5e50cc5488eaabf2a32
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 13:36:00 +0300

    Initial commit for add tweet

---
+++ src/components/AddTweet/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="add-tweet">
+    <div class="add-tweet-profile">
+      <img src="">
+    </div>
+    <div class="add-tweet-content">
+      <div class="text-section" />
+      <div class="controls">
+        <div class="controls-media">
+          <div class="controls-media-item">
+            <base-icon />
+          </div>
+        </div>
+        <div class="controls-submit">
+          <button>Tweet</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseIcon from '@/components/BaseIcon'
+export default {
+  name: 'AddTweet',
+  components:{
+    BaseIcon
+  }
+}
+</script>
+
+<style>
+
+</style>
