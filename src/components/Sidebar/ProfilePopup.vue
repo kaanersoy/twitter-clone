c3c3f3a81030654a50edd4d8df0e8bd885732513
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 22:42:04 +0300

    add sidebar profile popup menu

--- src/components/Sidebar/ProfilePopup.vue
+++ src/components/Sidebar/ProfilePopup.vue
@@ -1,38 +1,88 @@
 <template>
-  <div class="sidebar-profile">
-    <div class="sidebar-profile-pic">
-      <img
-        :src="me.profile.pic"
-      >
-    </div>
-    <div class="sidebar-profile-items">
-      <div class="profile-info">
-        <p>{{ me.profile.name }}</p>
-        <p class="nickname">
-          {{ me.profile.nickname }}
-        </p>
+  <div
+    class="sidebar-profile-wrapper"
+  >
+    <div
+      class="sidebar-profile"
+      @click="toggleMenu"
+    >
+      <div class="sidebar-profile-pic">
+        <img
+          :src="me.profile.pic"
+        >
       </div>
-      <div class="more">
-        <base-icon icon="more" />
+      <div class="sidebar-profile-items">
+        <div class="profile-info">
+          <p>{{ me.profile.name }}</p>
+          <p class="nickname">
+            {{ me.profile.nickname }}
+          </p>
+        </div>
+        <div class="more">
+          <base-icon icon="more" />
+        </div>
       </div>
     </div>
-    <div class="sidebar-profile-popup" />
+    <div
+      v-if="isMenuOpened"
+      class="sidebar-profile-popup"
+    >
+      <div class="popup-header">
+        <div class="sidebar-profile-pic">
+          <img
+            :src="me.profile.pic"
+          >
+        </div>
+        <div class="sidebar-profile-items">
+          <div class="profile-info">
+            <p>{{ me.profile.name }}</p>
+            <p class="nickname">
+              {{ me.profile.nickname }}
+            </p>
+          </div>
+          <div class="more">
+            <base-icon icon="tick" />
+          </div>
+        </div>
+      </div>
+      <hr class="popup-spacing">
+      <div class="popup-body">
+        <div class="popup-body-item">
+          <p>Add an existing account</p>
+        </div>
+        <hr class="popup-spacing">
+        <div class="popup-body-item">
+          <p>Logout from <span>{{ me.profile.nickname }}</span></p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import BaseIcon from '@/components/BaseIcon'
+import BaseIcon from '../BaseIcon/index'
 
 export default {
   name: 'ProfilePopup',
-  components: [
+  components: {
     BaseIcon
-  ],
+  },
+  data: function(){
+    return {
+      isMenuOpened: false
+    }
+  },
   computed: {
     ...mapGetters({
       me: 'getMe'
     })
+  },
+  methods:{
+    toggleMenu: function(){
+      this.isMenuOpened = !this.isMenuOpened
+      console.log(this.isMenuOpened);
+    }
   },
 }
 </script>
@@ -44,13 +94,20 @@ export default {
   display: flex;
   align-items: center;
   width: 100%;
-  padding: 0 1rem;
+  padding: 4px 1rem;
+  margin-bottom: 10px;
+  cursor: pointer;
   border-radius: 999px;
+  &-wrapper{
+    position: relative;
+  }
   &:hover{
     background-color: rgba($color: $color-blue, $alpha: 0.3);
   }
   &-pic{
     width: 3rem;
+    height: 3rem;
+    margin: 6px 0;
     img{
       border-radius: 999px;
       width: 100%;
@@ -63,7 +120,12 @@ export default {
     align-items: center;
     justify-content: space-between;
     .profile-info{
+      user-select: none;
       p{
+        margin: 0;
+        & + p {
+          margin-top: 5px;
+        }
         color: #fff;
         &.nickname{
           color: $color-dark-gray;
@@ -79,6 +141,54 @@ export default {
         display: block;
       }
     }
-  } 
+  }
+  &-popup{
+    position: absolute;
+    z-index: 3;
+    width: 105%;
+    bottom: calc(100% + 20px);
+    left: 0;
+    border-radius: 1rem;
+    padding: 10px;
+    background-color: $color-bg;
+    box-shadow: $shadow-white;
+    & .popup-spacing{
+      border: none;
+      background-color: rgba($color: $color-dark-gray, $alpha: 0.2);
+      padding-top: 2px;
+      margin: 3px;
+    }
+    .popup-header{
+      display: flex;
+      width: 100%;
+      padding: 8px;
+      .more{
+        width: 1.5rem;
+        height: 1.5rem;
+        svg{
+          fill: $color-blue;
+          width: 100%;
+        }
+      }
+    }
+    .popup-body{
+      &-item{
+        color: #fff;
+        font-size: 1.1rem;
+        padding: 1rem;
+        cursor: pointer;
+        &:hover{
+          background-color: rgba($color: $color-dark-gray, $alpha: 0.3);
+        }
+        p{
+          margin: 0;
+          padding: 0;
+        }
+        span{
+          color: $color-blue;
+        }
+      }
+    }
+  }
 }
 </style>
