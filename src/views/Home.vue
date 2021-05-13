eb0f6a4db020726c1ac3561a3bff6f1237de821c
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-13 22:42:54 +0300

    Update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-    <h1>
-      Home
-    </h1>
+  <div class="home">
+    <add-tweet />
+    <hr class="gap">
+    <div class="tweet-wrapper">
+      <tweet />
+    </div>
   </div>
 </template>
 
 <script>
+import AddTweet from '@/components/AddTweet'
+import Tweet from '@/components/Tweet'
 export default {
-
+  components: {
+    AddTweet,
+    Tweet
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/assets/theme/colors.scss';
 
+hr.gap{
+  background-color: $hr-color;
+  padding: 4px 0;
+  margin: 0;
+  border: none;
+}
 </style>
