162092c9028620033209f24c2bdcece2b91ff78b
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 03:05:10 +0300

    Add navigation guard for login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 
 const routes = [
   {
@@ -6,11 +7,31 @@ const routes = [
     name: 'Home',
     component: () => import('@/views/Home.vue')
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters.getLoginStatus
+  console.log(isLoggedIn);
+  if(isLoggedIn === false){
+    if(to.name !== 'Login'){
+      next({path: '/login'})
+    }
+    else {
+      next()
+    }
+  }
+  else next()
+})
+
 
 export default router
