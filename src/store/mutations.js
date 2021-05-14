783ddc44d3b7014cfe2033b46f9246df2b5729dc
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 12:49:39 +0300

    Add login mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,14 @@
 export default {
   toggleTweetButton(state){
     state.isTweetPopupActive = !state.isTweetPopupActive
+  },
+  setLoadingStatus(state, payload){
+    state.globalIsLoading = payload
+  },
+  setMe(state, payload){
+    state.me = payload
+  },
+  setLoginStatus(state, payload){
+    state.isLoggedIn = payload
   }
 }
