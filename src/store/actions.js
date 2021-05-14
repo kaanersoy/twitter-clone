ac2cb4ba7cfab2c6f5463512fa6c71b850108e48
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 12:48:50 +0300

    Add loginInfo and logout store actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,10 @@
-export default {}
+export default {
+  setLoginInfo({commit}, payload){
+    commit('setMe', payload);
+    commit('setLoginStatus', true)
+  },
+  setLogOut({commit}){
+    // commit('setMe', {})
+    commit('setLoginStatus', false)
+  }
+}
