8a6dd9ddee261295728063291f25e97cb8df0159
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 12:49:03 +0300

    Add loggedIn and me state

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,6 @@
 export default {
-  me: {
-    profile: {
-      pic: 'https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_normal.jpg',
-      nickname: '@kaanersoy',
-      name: 'Kaan Ersoy'
-    }
-  },
+  me: {},
   isTweetPopupActive: false,
-  isLoggedIn: false
+  isLoggedIn: false,
+  globalIsLoading: false
 }
