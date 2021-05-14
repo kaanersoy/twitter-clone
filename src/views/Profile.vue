4f64b160a58a21056397f17ec7014bccf6285425
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 13:45:07 +0300

    Initial commit for profile view

---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    <div class="profile-wrapper">
+      <h1>{{ $route.params.profileId ? $route.params.profileId : 'yok' }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileView'
+}
+</script>
+
+<style>
+
+</style>
