50127fbe4ab0e250aeceb7b9342d097438276be4
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-14 12:59:34 +0300

    Move mockdata to mockdata.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,9 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import store from '@/store'
+import {userOneAuthInfo, users} from './mockdata'
 
 const mock = new MockAdapter(axios, {delayResponse: 500});
-
-const users = [
-  {
-    id: '12a1s3dz24c635q2e213a2dw1',
-    username: 'kaanersoy',
-    password: 'password',
-    profile: {
-      pic: 'https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_normal.jpg',
-      nickname: '@kaanersoy',
-      name: 'Kaan Ersoy'
-    }
-  }
-]
-const userOneAuthInfo = {
-  username: users[0].username,
-  password: users[0].password
-}
 
 mock.onPost("/auth", userOneAuthInfo).reply(200, {user: users[0]});
 
---
+++ src/services/mockdata.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: '12a1s3dz24c635q2e213a2dw1',
+    username: 'kaanersoy',
+    password: 'password',
+    profile: {
+      pic: 'https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_normal.jpg',
+      nickname: '@kaanersoy',
+      name: 'Kaan Ersoy'
+    }
+  }
+]
+export const userOneAuthInfo = {
+  username: users[0].username,
+  password: users[0].password
+}
