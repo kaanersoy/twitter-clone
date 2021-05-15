3b8fde76f5e6cae0e4f7ba4eeb6d76af1db486e6
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-15 17:13:58 +0300

    Add notification custom method

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { notification } from '@/utils/notification'
 
 const app = createApp(App)
+
+app.config.globalProperties.$notification = notification
+
 app.use(router)
 app.use(store)
 app.mount('#app')
