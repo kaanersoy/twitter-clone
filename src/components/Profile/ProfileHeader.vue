57f67e102a4d1be48bff03f1251c4c19c66e6400
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-16 12:55:27 +0300

    Update css for profileHeader component

--- src/components/Profile/ProfileHeader.vue
+++ src/components/Profile/ProfileHeader.vue
@@ -95,6 +95,98 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import '@/assets/theme/colors.scss';
+.profile{
+      &-actions{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &-image{
+          width: 130px;
+          height: 130px;
+          margin-top: -80px;
+          img{
+            border: 1px solid $color-dark-gray;
+            border-radius: 999px;
+            width: 100%;
+          }
+        }
+        &-edit{
+          .edit-button{
+            border-radius: 999px;
+            border: 1px solid $color-blue;
+            color: $color-blue;
+            font-weight: bold;
+            font-size: 1rem;
+            padding: 1rem;
+            cursor: pointer;
+            transition: background-color 80ms ease;
+            &:hover{
+              background-color: rgba($color: $color-blue, $alpha: 0.1);
+            }
+          }
+        }
+      }
+      &-info{
+        margin-top: 1rem;
+        &-name{
+          color: #fff;
+          margin: 0;
+          font-weight: bold;
+          font-size: 1.5rem;
+        }
+        &-username{
+          font-size: 1.2rem;
+          color: $color-dark-gray;
+        }
+      }
+      &-description{
+        margin-top: 1rem;
+        color: #fff;
+      }
+      &-created-at{
+        margin-top: 1rem;
+        display: flex;
+        align-items: center;
+        color: $color-dark-gray;
+        a{
+          color: $color-blue;
+          &:hover{
+            text-decoration: underline;
+          }
+        }
+        span{
+          display: flex;
+          align-items: center;
+          & + span{
+            margin-left: 2rem;
+          }
+          svg{
+            fill: $color-dark-gray;
+            margin-right: .5rem;
+            width: 1.2rem;
+            height: 1.2rem;
+          }
+        }
+      }
+      &-follower-counts{
+        display: flex;
+        color: #fff;
+        cursor: pointer;
+        margin-top: 1rem;
+        p{
+          margin: 0;
+          & + p{
+            margin-left: 1rem;
+          }
+          span{
+            color: $color-dark-gray;
+          }
+          &:hover{
+            text-decoration: underline;
+          }
+        }
+      }
+    }
 </style>
