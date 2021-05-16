dc7e9a207f252440d5eac9c691d43ace04d534aa
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-16 12:51:46 +0300

    Add account & profile info

--- src/services/mockdata.js
+++ src/services/mockdata.js
@@ -8,8 +8,16 @@ const userData = [
     password: 'password',
     profile: {
       pic: 'https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_normal.jpg',
+      pic_full: 'https://pbs.twimg.com/profile_images/1344794962697793537/GYEm82zC_400x400.jpg',
+      pic_cover: 'https://pbs.twimg.com/profile_banners/1142171408916762624/1605471218/1500x500',
+      description: 'Somedescription goes there...',
       nickname: '@kaanersoy',
-      name: 'Kaan Ersoy'
+      name: 'Kaan Ersoy',
+      website: 'https://kaanersoy.com'
+    },
+    account: {
+      followingCount: '515',
+      followerCount: '152'
     }
   }
 ]
