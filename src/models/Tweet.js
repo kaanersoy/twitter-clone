b83b7f328a0cffe8709a905f27b934d4b819a2ad
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-18 21:16:58 +0300

    Add nanoid and integrate for Tweet model

--- src/models/Tweet.js
+++ src/models/Tweet.js
@@ -1,5 +1,8 @@
+import { nanoid } from "nanoid";
+
 export default class {
   constructor(author, content){
+    this.id = nanoid()
     this.author = author;
     this.content = content;
     this.createdAt = new Date().getTime();
